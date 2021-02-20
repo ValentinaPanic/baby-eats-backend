73015c86491bb91824812db5e8b56b20d661fff8
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-20 15:35:52 -0700

    render serialized instances

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
     
         if @user && @user.authenticate(params[:session][:password])
           session[:user_id] = @user.id
-          # render json: UserSerializer.new(@user), status: :ok
-          render json: @user
+          render json: UserSerializer.new(@user), status: :ok
+          # render json: @user
         else
           render json: {
             error: "Invalid Credentials"
@@ -16,8 +16,8 @@ class SessionsController < ApplicationController
       def get_current_user
        
         if logged_in?
-          render json: current_user
-          #  UserSerializer.new(current_user)
+          render json: UserSerializer.new(current_user)
+          #  
         else
           render json: {
             error: "No one logged in"
