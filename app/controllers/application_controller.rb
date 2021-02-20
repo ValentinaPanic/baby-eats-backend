df98a57b5ca6798ee4a4db063e097e95b8a2394e
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-19 19:51:15 -0700

    set the session id for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
+    include ::ActionController::Cookies
 
     def current_user
-        User.first
+        User.find_by(id: session[:user_id])
     end
 
     def logged_in?
