e8488050ac1ae72e864225caf9cc42095d846390
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-25 21:11:47 -0700

    signup route

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_221521) do
+ActiveRecord::Schema.define(version: 2021_02_26_035233) do
 
   create_table "days", force: :cascade do |t|
     t.date "date"
     t.integer "week_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "meal_type"
   end
 
   create_table "foods", force: :cascade do |t|
