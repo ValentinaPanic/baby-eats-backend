c38e9942894ca92073e8abcdfe85c79269469506
ValentinaPanic <valentina.panic87@gmail.com>
2021-03-05 17:35:53 -0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  
   post "/login", to: "sessions#create"
   post "/signup", to: "users#create"
   delete "/logout", to: "sessions#destroy"
   get "/get_current_user", to: "sessions#get_current_user"
 
   resources :foods
-  resources :days
+  resources :days do
+    resources :foods
+  end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
