eaae1deb4b8c9d0a65fd622ec2617e13eddb857f
ValentinaPanic <valentina.panic87@gmail.com>
2021-03-11 17:49:37 -0700

    list table and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_221521) do
+ActiveRecord::Schema.define(version: 2021_03_10_030423) do
 
   create_table "days", force: :cascade do |t|
     t.date "date"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_02_18_221521) do
     t.string "name"
     t.string "meal_type"
     t.integer "day_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
